78bd74b9b7aaa322296000f3a853b39f801372ef
Sam <sam@twothreefall.co.uk>
2011-10-16 14:06:10 +0100

    Week data parsing

--- lastfmexplorer/tests.py
+++ lastfmexplorer/tests.py
@@ -1,11 +1,11 @@
-import os
 from django.utils import unittest
 
 import tasks, requester
 
 class XMLHandling(unittest.TestCase):
-
+    """Tests for valid Last.fm XML files"""
     def __init__(self, methodName):
+        import os
         unittest.TestCase.__init__(self, methodName)
         path = os.path.dirname(__file__)
         path = os.path.join(path, "test-data")
@@ -17,6 +17,12 @@ class XMLHandling(unittest.TestCase):
                         (1109505601, 1110110401), (1110110401, 1110715201)]
         self.assertListEqual(chartList, expected)
 
+    def testWeekDataParsing(self):
+        data = tasks.week_data('aradnuk', self.requester, 1109505601, 1110110401)
+        self.assertEqual(len(data.keys()), 74)
+        # .. assertions against contents of db
+
 
 class RequestErrorHandling(unittest.TestCase):
+    """.. and ones for invalid Last.fm XML files"""
     pass
