c5c2a5aa5cd6a0ccab69cb5be48338a5132b2d5e
Sam <aradnuk@gmail.com>
2012-04-03 22:35:38 +0100

    Drop twothreefall module from lex breadcrumbs and use url prefix in urls.py

--- lastfmexplorer/urls.py
+++ lastfmexplorer/urls.py
@@ -13,38 +13,40 @@ __year_matcher  = r'(?P<year>\d{4})/$'
 __default_dates = { 'start' : ldates.idx_beginning,
                     'end'   : ldates.idx_last_sunday }
 
-urlpatterns = patterns('',
-    # start and page
-    (r'^$', views.start),
-    (__user_base + 'index/$', views.user_data),
+urlpatterns = patterns('lastfmexplorer.views',
+    # start
+    (r'^$', 'start'),
 
     # updates
-    (__user_base + 'update/$', views.update),
-    (r'^poll-update$', views.poll_update_status),
+    (__user_base + 'update/$', 'update'),
+    (r'^poll-update$', 'poll_update_status'),
 
     # invalid XML
-    (r'^bad-weeks$', views.list_bad_xml_files),
+    (r'^bad-weeks$', 'list_bad_xml_files'),
 
     # plain user overview, first between two dates, second all time.
-    (__user_base + __date_matcher, views.overview),
-    (__user_base + __year_matcher, views.overview),
-    (__user_base + '$', views.overview, __default_dates),
+    (__user_base + __date_matcher, 'overview'),
+    (__user_base + __year_matcher, 'overview'),
+    (__user_base + '$', 'overview', __default_dates),
 
     # single week chart
     (__user_base + r'chart/week/(?P<start>\d*)/$', views.user_week_chart),
 
     # top artist charts
-    (__user_base + r'chart/' + __date_matcher, views.user_chart),
-    (__user_base + r'chart/' + __year_matcher, views.user_chart),
-    (__user_base + r'chart/$', views.user_chart, __default_dates),
+    (__user_base + r'chart/' + __date_matcher, 'user_chart'),
+    (__user_base + r'chart/' + __year_matcher, 'user_chart'),
+    (__user_base + r'chart/$', 'user_chart', __default_dates),
 
     # suggest a listen
     (__user_base + r'who/$', views.who),
 
     # user/artist combos
-    (__user_base + r'artists/(?P<artists>.*)/$', views.user_and_artist, __default_dates),
-    (__user_base + r'artists/(?P<artists>.*)/' + __date_matcher, views.user_and_artist),
-    (__user_base + r'artists/(?P<artists>.*)/' + __year_matcher, views.user_and_artist),
+    (__user_base + r'artists/(?P<artists>.*)/$', 'user_and_artist', __default_dates),
+    (__user_base + r'artists/(?P<artists>.*)/' + __date_matcher, 'user_and_artist'),
+    (__user_base + r'artists/(?P<artists>.*)/' + __year_matcher, 'user_and_artist'),
+
+    # user chart index
+    (__user_base + 'index/$', 'user_data'),
 
 )
 
